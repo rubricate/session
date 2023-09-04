dbd384885b9d429690cd63d4e8b45625993f254f
Estefanio N Santos <estefanions@gmail.com>
2023-09-04 20:42:14 -0300

    PSR-12: Coding Style

--- Rubricate/Session/StorageSession.php
+++ Rubricate/Session/StorageSession.php
@@ -1,22 +1,9 @@
 <?php
 
-/*
- * @package     RubricatePHP
- * @author      Estefanio NS <estefanions AT gmail DOT com>
- * @link        http://rubricate.github.io
- * @copyright   2016
- * 
- */
-
 namespace Rubricate\Session;
-
-
 
 class StorageSession
 {
-
-
-
     public function start()
     {
         if(!self::hasStart()) {
@@ -26,8 +13,6 @@ class StorageSession
         return $this;
     }  
 
-
-
     public function cacheExpire($num)
     {
         if(is_numeric($num)) {
@@ -36,8 +21,6 @@ class StorageSession
 
         return $this;
     } 
-    
-
 
     public function delAll() 
     {
@@ -47,22 +30,16 @@ class StorageSession
         return $this;
     }
 
-
-
     public function set($key, $value)
     {
         $_SESSION[$key] = $value;
         return $this;
     }
 
-
-
     public function get($key, $default = '') 
     {
         return (!self::has($key))? $default : $_SESSION[$key];
     }
-
-
 
     public function del($key) 
     {
@@ -74,42 +51,30 @@ class StorageSession
         return false;
     }
 
-
-
     public function getId() 
     {
         return session_id();
     }
-
-
 
     public function getAll()
     {
         return $_SESSION;
     }
 
-
-
     public function has($key) 
     {
         return array_key_exists($key, self::getAll());
     }
-
-
 
     public function hasInArr($key, $valueValidArr) 
     {
         return ( in_array(self::get($key), $valueValidArr) );
     }
 
-
-
     private function hasStart()
     {
         return (self::getId() !== '');
     } 
-    
-
 
 }
 
